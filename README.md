3252f361c7f113a19dd9f648fb2da8e2d212aa69
Icarodgl <icarodgl@gmail.com>
2019-07-05 17:40:33 -0300

    feat(desenho): fiz tudo!

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# trabalho_c2
+# Trabalho de Calculo II
+
+## Integrantes
+    - Icaro Duarte Gavazza Lima
+    - Magno Macedo de Oliveira
+
+## Orientadora
+    - Adriana Padua Lovatte
+
+
+# Curvas paramétricas
+### Criação de animação com curvas paramétricas
+
+Utilizando ferramentas front-end para criar uma animação com curvas parametrizadas
--- main.js
+++ main.js
@@ -2,10 +2,15 @@ const x_width = window.innerWidth;
 const x_Height = window.innerHeight;
 const x_centro = x_width / 2;
 const y_centro = x_Height / 2;
-const frames = 60;
+const frames = 3000;
 const mh = document.getElementById("desenho").setAttribute("height", x_Height);
 const mw = document.getElementById("desenho").setAttribute("width", x_width);
-
+const mc = document.getElementById("cara_fundo")
+mc.setAttribute("cy", y_centro);
+mc.setAttribute("cx", x_centro);
+const mb = document.getElementById("cara_fundo")
+mb.setAttribute("cy", y_centro);
+mb.setAttribute("cx", x_centro);
 desenho();
 
 function sleep(time) {
@@ -14,30 +19,90 @@ function sleep(time) {
 
 function cria_linha(subdivide) {
   let lista = [];
+  const tamanho = subdivide/2
   for (let index = 0; index < subdivide; index++) {
-    lista.push({ x: subdivide / 2 - index, y: 0 });
+
+    lista.push({ x: index - tamanho/2, y: index - tamanho/2 });
   }
   return lista;
 }
 
-function desenha_curva(lista, frame) {
+function desenha_olho_esq(lista, frame, id) {
   pontos = "";
-  lista.map(p => {
-    nx = p.x;
-    ny = Math.pow(p.x, frame);
-    pontos += `${nx + x_centro},${ny + y_centro} `;
-  });
-  return pontos;
+  const tamanho = lista.length
+  
+  for (let index = 0; index < tamanho; index++) {
+    const p = lista[index];
+    let theta = index/2 - tamanho
+
+    nx =  (Math.cos((theta+frame))* index)+80;
+    ny = (Math.sin((theta+frame))* index)+50;
+    pontos += `${nx + x_centro},${-ny + y_centro} `;
+  }
+
+  document.getElementById(id).setAttribute("points", pontos);
 }
-async function anima(frames, lista) {
+
+function desenha_olho_dir(lista, frame, id) {
+  pontos = "";
+  const tamanho = lista.length
+  
+  for (let index = 0; index < tamanho; index++) {
+    const p = lista[index];
+    let theta = index/2 - tamanho
+
+    nx = (Math.cos((theta-frame))* index)-80;
+    ny =  (Math.sin((theta-frame))* index)+50;
+    pontos += `${nx + x_centro},${-ny + y_centro} `;
+  }
+
+  document.getElementById(id).setAttribute("points", pontos);
+
+}function desenha_cara(lista,  id) {
+  pontos = "";
+  const tamanho = lista.length
+  
+  for (let index = 0; index < tamanho; index++) {
+    const p = lista[index];
+    let theta = index/10 - tamanho/4
+
+    nx = Math.cos(theta)*200;
+    ny = Math.sin(theta)*200;
+    pontos += `${nx + x_centro},${-ny + y_centro} `;
+  }
+
+  document.getElementById(id).setAttribute("points", pontos);
+}
+function desenha_boca(lista,frame, id) {
+  pontos = "";
+  const tamanho = lista.length
+  
+  for (let index = 0; index < tamanho; index++) {
+    const p = lista[index];
+    let theta = index/2 - tamanho/4
+
+    nx = theta*8;
+    ny = Math.sin(theta)*Math.sin(frame)*6 - 80;
+    pontos += `${nx + x_centro},${-ny + y_centro} `;
+  }
+
+  document.getElementById(id).setAttribute("points", pontos);
+}
+async function anima(frames) {
+  var olho = cria_linha(50);
+  var cabeca = cria_linha(100)
+  desenha_cara(cabeca, "cara");
+  
   for (let index = 0; index < frames; index++) {
-    let pontos = desenha_curva(lista, index);
-    document.getElementById("linhas").setAttribute("points", pontos);
-    await sleep(500);
+    desenha_olho_esq(olho, index, "olho_esq");
+    desenha_olho_dir(olho, index, "olho_dir");
+    desenha_boca(olho, index,"boca")
+   
+    await sleep(200);
   }
 }
 
 function desenho() {
-  var lista = cria_linha(400);
-  anima(frames, lista);
+  
+  anima(frames);
 }
